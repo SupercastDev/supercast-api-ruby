3eb01daa2d983b88926ad1a0eea1841707650c89
Jody Heavener <j.heavener@gmail.com>
2019-08-19 15:26:53 -0700

    Rubocop fix

--- spec/lib/supercast/errors_spec.rb
+++ spec/lib/supercast/errors_spec.rb
@@ -5,7 +5,7 @@ require File.expand_path('../../../lib/supercast.rb', __dir__)
 RSpec.describe Supercast::SupercastError do
   describe 'self.initialize' do
     it 'correctly initializes' do
-      message = "Something went wrong"
+      message = 'Something went wrong'
 
       allow_any_instance_of(Supercast::SupercastError).to receive(:initialize).with(message)
 
